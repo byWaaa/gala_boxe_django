ef86005e6cd93fe41ddcd87455663a59031f677f
Monaim Farhane <2630723@etu.cchic.ca>
2026-09-17 14:53:58 -0400

    Configuration admin et urls de base.

--- gala/views.py
+++ gala/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Acceuil(request):
+    return render(request, 'gala/Acceuil.html')
--- gala_boxe/urls.py
+++ gala_boxe/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls.static import static
+
+from gala_boxe import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),  # Include the built-in auth URLs
+    path('', include('gala.urls')),  # Include the URLs from the gala app
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
