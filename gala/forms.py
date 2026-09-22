d550bae60e6a4f5afce16a3cb3cf168f05b49ddd
Monaim Farhane <2630723@etu.cchic.ca>
2026-09-22 10:29:23 -0400

    Formulaire CRUD

--- gala/forms.py
+++ gala/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Club
 
 class InscriptionForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,3 +9,13 @@ class InscriptionForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+
+
+class ClubForm(forms.ModelForm):
+    class Meta:
+        model = Club
+        fields = ['nom', 'ville', 'date_fondation', 'nombre_combattants', 'logo']
+        widgets = {
+            'date_fondation': forms.DateInput(attrs={'type': 'date'}),
+        }
